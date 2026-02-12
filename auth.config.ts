adcdf3b49e09fe8195ba15d2ee869f3c80b592d2
parvinbiti <parvinbiti365@gmail.com>
2026-02-12 04:33:16 -0300

    trustHost: true, // ✅ UntrustedHost error fix

--- auth.config.ts
+++ auth.config.ts
@@ -37,6 +37,8 @@ import type { NextAuthConfig } from 'next-auth'
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [],
+  trustHost: true, // ✅ UntrustedHost error fix
+
   callbacks: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     authorized({ request, auth }: any) {
